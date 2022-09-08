c6b9b60aac5555788884e659502de53e5a7f1351
Leonardo Villa <info@leographic.it>
2022-09-08 19:38:54 -0400

    rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -4,8 +4,30 @@
 public class Rectangle {
 
     double width,lenght;
-    
+
+    // Accessor (to access value)
+    public double getWidth(){
+        return width;
+    }
+    public double getLenght(){
+        return lenght;
+    }
+
+    // Mutators (to overide the value)
+    public void setWidth(double w){
+        width = w;
+    }
+    public void setLenght(double l){
+        lenght = l;
+    }
+
+    public double getArea(){
+        return width*lenght;
+    }
+
     public static void main(String[] args) {
-        
+        Rectangle r1 = new Rectangle();
+        r1.setWidth(5);
+        System.out.println("Width is " + r1.getWidth());
     }
 }
