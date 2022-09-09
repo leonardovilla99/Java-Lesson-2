ef09edcdbf8b6c17a826118a08da1346e8ef2c39
Leonardo Villa <info@leographic.it>
2022-09-08 20:27:30 -0400

    mod

--- TestRectangle.java
+++ TestRectangle.java
@@ -1,6 +1,6 @@
 public class TestRectangle {
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle();
+        Rectangle r1 = new Rectangle(3,1);
         r1.width = 10;
         r1.setWidth(10);
     }
